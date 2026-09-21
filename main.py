c36cd05633a623cf215827620c654ce2adf7f521
Alberto Franca <albertofranca41@gmail.com>
2026-09-21 10:19:49 -0300

    Extract models into separate module

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
 import uuid
+from models import Subject, Topic, Note
 from datetime import datetime, timezone
-
-# ===========================================
-#   Creating the classes
-# ===========================================
-
-class Subject:
-    def __init__(self, id, name, created_at, updated_at):
-        self.id = id
-        self.name = name
-        self.created_at = created_at
-        self.updated_at = updated_at
-
-
-class Topic:
-    def __init__(self, id, name, subject_id, created_at, updated_at):
-        self.id = id
-        self.name = name
-        self.subject_id = subject_id
-        self.created_at = created_at
-        self.updated_at = updated_at
-
-
-class Note:
-    def __init__(self, id, title, content, topic_id, created_at, updated_at):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.topic_id = topic_id
-        self.created_at = created_at
-        self.updated_at = updated_at
 
 # ===========================================
 #   Objects for testing
