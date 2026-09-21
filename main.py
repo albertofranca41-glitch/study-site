3499ee42bbe15f60ad6ad878212c358d3c9c073c
Alberto Franca <albertofranca41@gmail.com>
2026-09-21 12:03:01 -0300

    Add CRUD execution flow

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import uuid
-from datetime import datetime, timezone
-
-from models import Subject, Topic, Note
 from services import (
     find_subject,
     find_topic,
@@ -17,9 +13,7 @@ from services import (
     delete_note,
 )
 
-# ===========================================
-#   Objects for testing
-# ===========================================
+# ==============================================
 
 subjects = []
 
@@ -28,3 +22,65 @@ topics = []
 
 
 notes = []
+
+# ===============================================
+# CRUD OPERATIONS
+# ===============================================
+
+def main():
+    subject_id = subject_crud()
+    topic_id = topic_crud(subject_id)
+    note_id = note_crud(topic_id)
+
+    delete_note_flow(note_id)
+    delete_topic_flow(topic_id)
+    delete_subject_flow(subject_id)
+
+def subject_crud():
+    subject = create_subject(subjects, "Subject Name")
+    print(subject.name)
+
+    found_subject = find_subject(subjects, subject.id)
+    print(found_subject.name)
+
+    update_subject(subjects, subject.id, "Updated Subject")
+    return subject.id
+
+def topic_crud(subject_id):
+    topic = create_topic(subjects, topics, subject_id, "Topic Name")
+    print(topic.name)
+
+    found_topic = find_topic(topics, topic.id)
+    print(found_topic.name)
+
+    update_topic(topics, topic.id, "Updated Topic")
+    return topic.id
+
+def note_crud(topic_id):
+
+    note = create_note(topics, notes, topic_id, "Note Title", "Note Content")
+    print(note.title)
+
+    found_note = find_note(notes, note.id)
+    print(found_note.title)
+
+    update_note(notes, note.id, "Updated Title", "Updated Content")
+    return note.id
+
+# ===============================================
+# DELETE FUNCTIONS
+# ===============================================
+
+def delete_subject_flow(subject_id):
+    delete_subject(subjects, topics, notes, subject_id)
+
+def delete_topic_flow(topic_id):
+    delete_topic(topics, notes, topic_id)
+
+def delete_note_flow(note_id):
+    delete_note(notes, note_id)
+
+# ------------------------------------------------
+
+if __name__ == "__main__":
+    main()
